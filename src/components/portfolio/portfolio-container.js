5f78394ed698daa88b3c543957c0bc3515dd4fdb
lorettalw <102196131+lorettalw@users.noreply.github.com>
2022-09-12 11:18:16 -0400

    Made major updates to code adding 12 files and modified 7

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axios from 'axios';
+
 import PortfolioItem from "./portfolio-item";
 
 
@@ -8,29 +10,75 @@ export default class PortfolioContainer extends Component {
 
         this.state = {
             pageTitle: "Welcome to my portfolio",
-            data: [
-            {title:"Quip"},
-            {title: "Eventbright"},
-            {title: "Ministry Safe"}
-            ]
+            isLoading: false,
+            data: []
         };
+
+        this.handleFilter = this.handleFilter.bind(this);
+
     }
 
-    portfolioItems() {
-
-        return this.state.data.map(item => {
-            return <PortfolioItem title={item.title}/>;
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter;
+            })
         });
     }
 
+    getPortfolioItems() {
+        axios
+          .get('https://lorettawatson.devcamp.space/portfolio/portfolio_items')
+          .then (response => {
+        // handle success
+            this.setState ({
+                data: response.data.portfolio_items
+            });
+          })
+          .catch(error => {
+        // handle error
+            console.log(error);
+          });
+      }
+
+
+    portfolioItems() {
+        return this.state.data.map(item => {
+            return (
+              <PortfolioItem 
+                key={item.id}
+                item={item} 
+              /> 
+            );      
+        });
+    }
+
+    componentDidMount() {
+      this.getPortfolioItems();
+    }
+
     render() {
+        if (this.state.isLoading) {
+            return <div>Loading...</div>;
+        }
+
+
         return (
-            <div>
-                <h2>{this.state.pageTitle}</h2>
+            <div className="portfolio-items-wrapper">
+                <button className="btn" onClick={() => this.handleFilter('eCommerce')}>
+                    eCommerce
+                </button>
+                <button className="btn" onClick={() => this.handleFilter('Scheduling')}>
+                    Scheduling
+                </button>
+                <button className="btn" onClick={() => this.handleFilter('Enterprise')}>
+                    Enterprise
+                </button>
 
                 {this.portfolioItems()}
-
             </div>
+                
+
         )
     }
 }
