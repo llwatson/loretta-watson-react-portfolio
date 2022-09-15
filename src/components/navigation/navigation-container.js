83659acc3bd25bc37551fc5591ea3723338bf083
lorettalw <102196131+lorettalw@users.noreply.github.com>
2022-09-14 20:23:14 -0400

    Made changes to 5 files and created an additional 5 files

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -7,8 +7,8 @@ const NavigationComponent = props => {
   const dynamicLink = (route, linkText) => {
     return (
       <div className="nav-link-wrapper">
-        <NavLink to="/blog" activeClassName="nav-link-active">
-          Blog
+        <NavLink to={route} activeClassName="nav-link-active">
+         {linkText}
         </NavLink>
       </div>
     );
@@ -46,12 +46,18 @@ const NavigationComponent = props => {
 
         <div className="nav-link-wrapper">
           <NavLink to="/contact" activeClassName="nav-link-active">
+            Blog
+          </NavLink>
+        </div>
+
+        <div className="nav-link-wrapper">
+          <NavLink to="/blog" activeClassName="nav-link-active">
             Contact
           </NavLink>
         </div>
 
         {props.loggedInStatus === "LOGGED_IN" ? (
-          dynamicLink("/blog", "Blog")
+          dynamicLink("/portfolio-manager", "Portfolio Manager")
         ) : null}
       </div>
 
